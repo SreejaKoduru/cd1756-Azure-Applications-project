2353d9fb72256c0ee0f987a85ba741d66eb85d0a
sreeja koduru <2004sreejakoduru@gmail.com>
2026-03-12 19:09:54 +0530

    auth update

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -89,7 +89,7 @@ def authorized():
         result = _build_msal_app(cache=cache).acquire_token_by_authorization_code(
             request.args['code'],
             scopes=Config.SCOPE,
-            redirect_uri=url_for('authorized', _external=True))
+            redirect_uri=url_for('authorized', _external=True,_scheme='https'))
         if "error" in result:
             return render_template("auth_error.html", result=result)
         session["user"] = result.get("id_token_claims")
@@ -137,5 +137,5 @@ def _build_auth_url(authority=None, scopes=None, state=None):
     return _build_msal_app(authority=authority).get_authorization_request_url(
         scopes,
         state=state,
-        redirect_uri=url_for("authorized", _external=True)
+        redirect_uri=url_for("authorized", _external=True,_scheme='https')
     )
