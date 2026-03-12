34471dc71be953d3ea6b1931f7981ec3e64fa0ce
SreejaKoduru <2004sreejakoduru@gmail.com>
2026-03-12 22:33:55 +0530

    Update views.py

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -62,19 +62,32 @@ def post(id):
 def login():
     if current_user.is_authenticated:
         return redirect(url_for('home'))
+
     form = LoginForm()
+
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
+
+        # FAILED LOGIN
         if user is None or not user.check_password(form.password.data):
+            app.logger.warning("Failed login attempt for user: %s", form.username.data)
             flash('Invalid username or password')
             return redirect(url_for('login'))
+
+        # SUCCESSFUL LOGIN
+        app.logger.info("Successful login for user: %s", form.username.data)
+
         login_user(user, remember=form.remember_me.data)
+
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('home')
+
         return redirect(next_page)
+
     session["state"] = str(uuid.uuid4())
     auth_url = _build_auth_url(scopes=Config.SCOPE, state=session["state"])
+
     return render_template('login.html', title='Sign In', form=form, auth_url=auth_url)
 
 @app.route(Config.REDIRECT_PATH)  # Its absolute URL must match your app's redirect_uri set in AAD
